ac4aa8e6ba992e5e689e7c6c1f79fdd1226176b9
Donal Gaspard <lyr.x.informatique@gmail.com>
2023-01-06 18:03:51 +0100

    structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,18 @@
-import { useState } from "react";
-import "./App.css";
 import Counter from "./components/Counter";
 import Addition from "./components/Addition";
+import Division from "./components/Division";
 
-const App = () => {
-	const [nbr3, setNbr3] = useState(0);
-	const [nbr4, setNbr4] = useState(0);
-	const [divisionResult, setDivisionResult] = useState(0);
-	const [errorMessage, setErrorMessage] = useState("");
+import "./App.css";
 
-	const division = () => {
-		if (parseInt(nbr4) === 0) {
-			setErrorMessage("Division by zero is not allowed");
-			return;
-		}
-		setDivisionResult(parseInt(nbr3) / parseInt(nbr4));
-	};
-
-	return (
+const App = () => (
 		<div>
 			<h1>Calculatrice</h1>
 			<Counter />
 			<Addition />
-			<h2>Diviser 2 nombres</h2>
-			<input
-				placeholder="first number"
-				type="number"
-				value={nbr3}
-				onChange={(event) => setNbr3(event.target.value)}
-			/>
-			{nbr3}
-			{" / "}
-			<input
-				value={nbr4}
-				placeholder="second number"
-				type="number"
-				onChange={(event) => setNbr4(event.target.value)}
-			/>
-			{nbr4}
-			<button onClick={division}>{"="}</button>
-			{divisionResult}
-			<br></br>
-			{errorMessage}
+			<Division />
+			{/* <Multiplication /> */}
+			{/* <Soustraction /> */}
 		</div>
 	);
-};
 
 export default App;
--- src/components/Division.tsx
+++ src/components/Division.tsx
@@ -1,1 +1,42 @@
-import React, { useState } from "react";
+import React, {useState} from 'react';
+
+const Division = () => {
+  const [nbr3, setNbr3] = useState(0);
+  const [nbr4, setNbr4] = useState(0);
+  const [divisionResult, setDivisionResult] = useState(0);
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const division = () => {
+    if (nbr4 === 0) {
+      setErrorMessage("Division by zero is not allowed");
+      return;
+    }
+    setDivisionResult(nbr3 / nbr4);
+  };
+  return (
+    <>
+      <h2>Diviser 2 nombres</h2>
+      <input
+        placeholder="first number"
+        type="number"
+        value={nbr3}
+        onChange={(event) => setNbr3(parseInt(event.target.value))}
+      />
+      {nbr3}
+      {" / "}
+      <input
+        value={nbr4}
+        placeholder="second number"
+        type="number"
+        onChange={(event) => setNbr4(parseInt(event.target.value))}
+      />
+      {nbr4}
+      <button onClick={division}>{"="}</button>
+      {divisionResult}
+      <br></br>
+      {errorMessage}
+    </>
+  );
+};
+
+export default Division;
