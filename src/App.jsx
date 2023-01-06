c6e7f67b686c098e5c5bbcf1d63cc1b3d155665b
Donal Gaspard <lyr.x.informatique@gmail.com>
2023-01-06 16:33:30 +0100

    addition and division works

--- src/App.jsx
+++ src/App.jsx
@@ -1,68 +1,81 @@
-import { useState } from "react";
+import {useEffect, useState} from "react";
 import reactLogo from "./assets/react.svg";
 import "./App.css";
 
 const App = () => {
-	const [counter, setCounter] = useState(0);
-	const [number1, setNumber1] = useState(1);
-	const [number2, setNumber2] = useState(0);
-	const [nbr3, setNumber3] = useState(1);
-	const [nbr4, setNumber4] = useState(0);
-	const [result, setResult] = useState(0);
+  const [counter, setCounter] = useState(0);
+  const [number1, setNumber1] = useState(0);
+  const [number2, setNumber2] = useState(0);
+  const [nbr3, setNbr3] = useState(0);
+  const [nbr4, setNbr4] = useState(0);
+  const [result, setResult] = useState(0);
+  const [divisionResult, setDivisionResult] = useState(0);
+  const [errorMessage, setErrorMessage] = useState("");
 
-	const decrement = () => setCounter(counter - 1);
-	const increment = () => setCounter(counter + 1);
-	const addition = () => {
-		console.log("addition");
-		setResult(parseInt(number1) + parseInt(number2));
-	};
-	const division = () => {
-		console.log("division");
-		setResult(parseInt(nbr3) / parseInt(nbr4));
-	};
+  const decrement = () => setCounter(counter - 1);
+  const increment = () => setCounter(counter + 1);
 
-	return (
-		<div>
-			<h1>My Movie DB</h1>
-			<button onClick={decrement}>{"-"}</button>
-			{counter}
-			<button onClick={increment}>{"+"} </button>
-			<br></br>
-			<h2>Ajouter 2 nombres</h2>
-			<input
-				placeholder="first number"
-				type="number"
-				value={number1}
-				onChange={(event) => setNumber1(event.target.value)}
-			/>
-			{number1}
-			{" + "}
-			<input
-				placeholder="second number"
-				type="number"
-				onChange={(event) => setNumber2(event.target.value)}
-			/>
-			{number2} <button onClick={division}>{"="}</button>
-			{result}
-			<br></br>
-			<h2>Diviser 2 nombres</h2>
-			<input
-				placeholder="first number"
-				type="number"
-				value={nbr3}
-				onChange={(event) => setNumber3(event.target.value)}
-			/>
-			{nbr3}
-			{" / "}
-			<input
-				placeholder="second number"
-				type="number"
-				onChange={(event) => setNumber4(event.target.value)}
-			/>
-			{nbr4} <button onClick={division}>{"="}</button>
-			{result}
-		</div>
-	);
+
+  const addition = () => {
+    setResult(parseInt(number1) + parseInt(number2));
+  };
+
+  const division = () => {
+    if (parseInt(nbr4) === 0) {
+      setErrorMessage("Division by zero is not allowed");
+      return;
+    }
+      setDivisionResult(parseInt(nbr3) / parseInt(nbr4));
+  };
+
+  return (
+    <div>
+      <h1>My Movie DB</h1>
+      <button onClick={decrement}>{"-"}</button>
+      {counter}
+      <button onClick={increment}>{"+"} </button>
+      <br></br>
+      <h2>Ajouter 2 nombres</h2>
+      <input
+        placeholder="first number"
+        type="number"
+        value={number1}
+        onChange={(event) => setNumber1(event.target.value)}
+      />
+      {number1}
+      {" + "}
+      <input
+        value={number2}
+        placeholder="second number"
+        type="number"
+        onChange={(event) => setNumber2(event.target.value)}
+      />
+      {number2}
+      <button onClick={addition}>{"="}</button>
+      {result}
+      <br></br>
+      <h2>Diviser 2 nombres</h2>
+      <input
+        placeholder="first number"
+        type="number"
+        value={nbr3}
+        onChange={(event) => setNbr3(event.target.value)}
+      />
+      {nbr3}
+      {" / "}
+      <input
+        value={nbr4}
+        placeholder="second number"
+        type="number"
+        onChange={(event) => setNbr4(event.target.value)}
+      />
+      {nbr4}
+      <button onClick={division}>{"="}</button>
+      {divisionResult}
+      <br></br>
+      {errorMessage}
+    </div>
+  );
 };
 
 export default App;
