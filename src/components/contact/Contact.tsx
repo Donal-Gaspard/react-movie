26b2c2d17b72fd21727e71a31976b799d6e922a5
Rodolph <rodolphcopr@gmail.com>
2023-02-10 19:32:12 +0100

    contact

--- src/components/contact/Contact.tsx
+++ src/components/contact/Contact.tsx
@@ -1,43 +1,60 @@
-import React, {useState} from 'react';
+import React, { useState } from "react";
 
 interface Person {
- id:number;
-  name: string;
+	id: number;
+	name: string;
 }
 const Contact = () => {
-  const [contacts, setContacts] = useState<Person[]>([]); // c'est un objet personne par un tableau vide car on évite d'avoir une variable undefined / Entre les chevrons j'initialise
-  const [contact, setContact] = useState<Person |null >(null); // je connais le type personne mais là on a pas de personne // toujours penser usestate et valeur par défaut // set contact sert à le mettre dans la mémoire
-  const [counter, setCounter] = useState(1); // la valeur par défaut est à 1
+	const [contacts, setContacts] = useState<Person[]>([]); // c'est un objet personne par un tableau vide car on évite d'avoir une variable undefined / Entre les chevrons j'initialise
+	const [contact, setContact] = useState<Person | null>(null); // je connais le type personne mais là on a pas de personne // toujours penser usestate et valeur par défaut // set contact sert à le mettre dans la mémoire
+	const [counter, setCounter] = useState(1); // la valeur par défaut est à 1
 
- const addContact = () => {
-   if (contact) {
-     setContacts([...contacts, contact]); // C'est un spread operator pour les tableau et les objets //prends les personnes qui sont déjà dans le tableau et ajoute moi contact similaire à push
-     setContact(null); // il sert de mémoire tampon
-     setCounter(counter + 1); // ajout d'un identifant
-   }
-  }
+	const addContact = () => {
+		if (contact) {
+			setContacts([...contacts, contact]); // C'est un spread operator pour les tableau et les objets //prends les personnes qui sont déjà dans le tableau et ajoute moi contact similaire à push
+			setContact(null); // il sert de mémoire tampon
+			setCounter(counter + 1); // ajout d'un identifant
+		}
+	};
 
-  const removeContact = (id: number) => setContacts(contacts.filter((contact) => contact.id !== id)); // renvoie moi tout à part cet id
+	const removeContact = (id: number) =>
+		setContacts(contacts.filter((contact) => contact.id !== id)); // renvoie moi tout à part cet id
 
- const contactsRender = contacts.map((contact) => ( // boucle sur chaque personne, je peux définir un rendu pour chaque personne // key => un identifiant unique pour chaque élément
-    <li key={contact.id}>
-      <>
-        {contact.name}
-        <span onClick={()=> removeContact(contact.id)} style={{color: 'red', cursor:'pointer', padding: '10px'}}>X</span>
-      </>
-    </li>
-  ));
+	const contactsRender = contacts.map(
+		(
+			contact // boucle sur chaque personne, je peux définir un rendu pour chaque personne // key => un identifiant unique pour chaque élément
+		) => (
+			<li key={contact.id}>
+				<>
+					{contact.name}
+					<span
+						onClick={() => removeContact(contact.id)}
+						style={{
+							color: "red",
+							cursor: "pointer",
+							padding: "10px",
+						}}
+					>
+						X
+					</span>
+				</>
+			</li>
+		)
+	);
 
-  console.log('Contact')
+	console.log("Contact");
 
-  return (
+	return (
 		<div>
 			<h2>Contact</h2>
-      {
-        contacts.length > 0 &&
-        <h3>Nombre de contact : {contacts.length}</h3> // ajouter un s si le nombre est supérieur à 1
-      }
-
+			{
+				contacts.length > 1 ? (
+					<h3>Nombre de contacts : {contacts.length}</h3>
+				) : (
+					// ajouter un s si le nombre est supérieur à 1
+					<h3>Nombre de contact : {contacts.length}</h3>
+				) // ajouter un s si le nombre est supérieur à 1
+			}
 			<input
 				type="text"
 				onChange={(e) =>
@@ -50,7 +67,7 @@ const Contact = () => {
 			contact number {contacts.length}
 			<ul style={{ listStyle: "none" }}>{contactsRender}</ul>
 		</div>
-  );
+	);
 };
 
 export default Contact;
